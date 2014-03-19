38624d7cd1fc5b1225f35726018cefe12f4abdb5
Christian Vozar <christian@rogueethic.com>
2014-03-19 17:02:51 -0500

    Do not display API key in debug info

--- stackdriver_output.go
+++ stackdriver_output.go
@@ -128,7 +128,6 @@ func (so *StackdriverCustomMetricOutput) Run(or OutputRunner, h PluginHelper) (e
 			err = client.Send(so.gwm)
 			if err != nil {
 				so.or.LogMessage(fmt.Sprintf("[StackdriverCustomMetricOutput] API submission fail: %s\n", err))
-				so.or.LogMessage(fmt.Sprintf("header info: apikey[%s]", so.conf.ApiKey))
 			}
 			so.gwm = sd.NewGatewayMessage()
 		}
